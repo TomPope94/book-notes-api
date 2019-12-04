21f0adaa0f9e420da4de9ee647d3e033a297e826
tompope94 <tompope1994@gmail.com>
2019-12-04 08:45:53 +0000

    changes made

--- services/books/createBook.js
+++ services/books/createBook.js
@@ -1,6 +1,6 @@
-import uuid from "uuid";
-import * as dynamoDbLib from "../../libs/dynamodb-lib";
-import { success, failure } from "../../libs/response-lib";
+import uuid from 'uuid';
+import * as dynamoDbLib from '../../libs/dynamodb-lib';
+import { success, failure } from '../../libs/response-lib';
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
@@ -20,13 +20,13 @@ export async function main(event, context) {
       bookReview: {},
       bookProgression: {
         pagesRead: 0,
-        dateStarted: today.toString()
+        dateStarted: today.toLocaleDateString('en-US')
       }
     }
   };
 
   try {
-    await dynamoDbLib.call("put", params);
+    await dynamoDbLib.call('put', params);
     return success(params.Item);
   } catch (e) {
     return failure({ status: false });
