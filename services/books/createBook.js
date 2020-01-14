a2cefd32bd077319dc3a8b1522ae9073043009e6
tompope94 <tompope1994@gmail.com>
2020-01-14 20:17:21 +0000

    updates api

--- services/books/createBook.js
+++ services/books/createBook.js
@@ -1,10 +1,17 @@
-import uuid from "uuid";
-import * as dynamoDbLib from "../../libs/dynamodb-lib";
-import { success, failure } from "../../libs/response-lib";
+import uuid from 'uuid';
+import moment from 'moment';
+import * as dynamoDbLib from '../../libs/dynamodb-lib';
+import { success, failure } from '../../libs/response-lib';
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
-  const today = await Date.now();
+  let dateVar;
+  if (data.date) {
+    dateVar = moment(data.date, 'YYYYMMDD').format('YYYYMMDD');
+  } else {
+    dateVar = moment().format('YYYYMMDD');
+  }
+  const dateNum = parseInt(dateVar);
   const params = {
     TableName: process.env.booksTableName,
     Item: {
@@ -16,17 +23,13 @@ export async function main(event, context) {
       coverArt: data.coverArt,
       categories: data.categories,
       bookLanguage: data.bookLanguage,
-      bookNotes: {},
-      bookReview: {},
-      bookProgression: {
-        pagesRead: 0,
-        dateStarted: today
-      }
+      bookState: data.bookState,
+      dateCreated: dateNum
     }
   };
 
   try {
-    await dynamoDbLib.call("put", params);
+    await dynamoDbLib.call('put', params);
     return success(params.Item);
   } catch (e) {
     return failure({ status: false });
