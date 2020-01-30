fd7bd348bb43e12ec4d65d02545a9afd861ec11c
Tom Pope <Tpope@libertylondon.com>
2020-01-30 17:29:12 +0000

    adds payment endpoints

--- services/payments/startPayIntent.js
+++ services/payments/startPayIntent.js
@@ -3,22 +3,20 @@ import { calculateCost } from "../../libs/billing-lib";
 import { success, failure } from "../../libs/response-lib";
 
 export async function main(event, context) {
-  const { slots, source } = JSON.parse(event.body);
+  const { slots } = JSON.parse(event.body);
   const amount = calculateCost(slots);
-  const description = "Book slots";
 
   // Load our secret key from the  environment variables
   const stripe = stripePackage(process.env.stripeSecretKey);
 
   try {
-    await stripe.paymentIntents.create({
-      source,
+    const secret = await stripe.paymentIntents.create({
       amount: amount,
-      description,
       currency: "gbp"
     });
-    return success({ status: true });
+    return success({ status: true, secret: secret });
   } catch (e) {
+    console.error(e);
     return failure({ message: e.message });
   }
 }
